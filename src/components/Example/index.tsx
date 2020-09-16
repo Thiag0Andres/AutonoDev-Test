d04be4451a9c77f2a035048d7a591e373cf3c645
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2020-09-15 22:18:35 -0300

    Teste 1.0

---
+++ src/components/Example/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./styles.scss";
+
+const Example: React.FC = () => {
+  const example = "";
+  return <section className="example">{example}</section>;
+};
+
+export default Example;
